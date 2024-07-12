45a953a45897377a5f6a23b27373fc6387db66a2
ChrisKim <1515748548@qq.com>
2024-07-12 10:27:23 +0800

    [feat] 删除上下行检测

--- main.py
+++ main.py
@@ -7,16 +7,12 @@ import tracker
 
 if __name__ == '__main__':
     # 初始化进入和离开计数
-    down_count_person = 0
-    up_count_person = 0
-    down_count_vehicle = 0
-    up_count_vehicle = 0
+    count_person = 0
+    count_vehicle = 0
 
     # 初始化计数列表
-    frame_up_counts_person = []
-    frame_down_counts_person = []
-    frame_up_counts_vehicle = []
-    frame_down_counts_vehicle = []
+    frame_counts_person = []
+    frame_counts_vehicle = []
 
     # 初始化时间列表
     time_stamps = []
@@ -29,12 +25,12 @@ if __name__ == '__main__':
     detector = Detector()
 
     # 初始化视频写入对象
-    fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
-    out = cv2.VideoWriter('output_video.avi', fourcc, 20.0, (960, 540))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('output_video.mp4', fourcc, 20.0, (960, 540))
 
     # 打开视频
-    # capture = cv2.VideoCapture('./video/test.mp4')
-    capture = cv2.VideoCapture('./video/IMG.mov')
+    # capture = cv2.VideoCapture('./video/IMG_6481.mov')
+    capture = cv2.VideoCapture('./video/test.mp4')
     # 用于跟踪已经统计过的目标ID的集合
     counted_ids = set()
 
@@ -46,8 +42,8 @@ if __name__ == '__main__':
 
         # 缩小尺寸，1920x1080->960x540
         im = cv2.resize(im, (960, 540))
-        list_bboxs = []
 
+        list_bboxs = []
 
         # 检测目标
         bboxes = detector.detect(im)
@@ -68,15 +64,9 @@ if __name__ == '__main__':
 
             # 统计人流和车流
             if label == 'person':
-                if y1 < 540 / 2:
-                    up_count_person += 1
-                else:
-                    down_count_person += 1
+                count_person += 1
             elif label == 'vehicle':
-                if y1 < 540 / 2:
-                    up_count_vehicle += 1
-                else:
-                    down_count_vehicle += 1
+                count_vehicle += 1
 
             # 将已统计过的跟踪ID添加到集合中
             counted_ids.add(track_id)
@@ -90,29 +80,23 @@ if __name__ == '__main__':
         else:
             # 如果画面中没有bbox
             output_image_frame = im
+
         # 当前帧上下行目标数量
-        current_up_count_person = sum(
-            1 for bbox in list_bboxs if bbox[1] < 540 / 2 and bbox[4] == 'person')  # 上半部分人数量
-        current_down_count_person = sum(
-            1 for bbox in list_bboxs if bbox[1] >= 540 / 2 and bbox[4] == 'person')  # 下半部分人数量
-        current_up_count_vehicle = sum(
-            1 for bbox in list_bboxs if bbox[1] < 540 / 2 and bbox[4] == 'vehicle')  # 上半部分车数量
-        current_down_count_vehicle = sum(
-            1 for bbox in list_bboxs if bbox[1] >= 540 / 2 and bbox[4] == 'vehicle')  # 下半部分车数量
-
-
+        current_count_person = sum(
+            1 for bbox in list_bboxs if bbox[4] == 'person')
+        current_count_vehicle = sum(
+            1 for bbox in list_bboxs if bbox[4] == 'vehicle')
 
         # 收集当前时间和帧计数
         current_time = time.time() - start_time
         time_stamps.append(current_time)
-        frame_up_counts_person.append(current_up_count_person)
-        frame_down_counts_person.append(current_down_count_person)
-        frame_up_counts_vehicle.append(current_up_count_vehicle)
-        frame_down_counts_vehicle.append(current_down_count_vehicle)
 
+        # 收集当前帧上下行目标数量
+        frame_counts_person.append(current_count_person)
+        frame_counts_vehicle.append(current_count_vehicle)
 
         # 绘制统计信息到帧上
-        text_draw = f'DOWN: Person {down_count_person} , Vehicle {down_count_vehicle} | UP: Person {up_count_person} , Vehicle {up_count_vehicle}'
+        text_draw = f'Person: {count_person}, Vehicle: {count_vehicle}.'
         output_image_frame = cv2.putText(img=im, text=text_draw,
                                          org=draw_text_position,
                                          fontFace=font_draw_number,
@@ -129,24 +113,20 @@ if __name__ == '__main__':
 
     # 生成柱状图
     plt.figure(figsize=(10, 5))
-    plt.bar(['Up Person', 'Down Person', 'Up Vehicle', 'Down Vehicle'],
-            [up_count_person, down_count_person,
-             up_count_vehicle, down_count_vehicle],
-            color=['blue', 'cyan', 'green', 'yellow'])
-    plt.title('Total Up and Down Counts for Person and Vehicle')
+    plt.bar(['Person', 'Vehicle'],
+            [count_person, count_vehicle],
+            color=['blue', 'cyan'])
+    plt.title('Total Counts for Person and Vehicle')
     plt.savefig('total_bar_chart.png')
     plt.show()
 
     # 生成折线图
     plt.figure(figsize=(10, 5))
-    plt.plot(time_stamps, frame_up_counts_person,
-             label='Up Person per Frame', color='blue')
-    plt.plot(time_stamps, frame_down_counts_person,
-             label='Down Person per Frame', color='cyan')
-    plt.plot(time_stamps, frame_up_counts_vehicle,
-             label='Up Vehicle per Frame', color='green')
-    plt.plot(time_stamps, frame_down_counts_vehicle,
-             label='Down Vehicle per Frame', color='yellow')
+    plt.plot(time_stamps, frame_counts_person,
+             label='Person per Frame', color='blue')
+    plt.plot(time_stamps, frame_counts_vehicle,
+             label='Vehicle per Frame', color='cyan')
+
     plt.xlabel('Time (s)')
     plt.ylabel('Counts per Frame')
     plt.title('Counts per Frame Over Time for Person and Vehicle')
