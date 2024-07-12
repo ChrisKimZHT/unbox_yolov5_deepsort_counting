0559e6bec44ebf23fda0aa0253a59e3aa7a2e635
ChrisKim <1515748548@qq.com>
2024-07-12 10:21:07 +0800

    [feat] 修改

--- main.py
+++ main.py
@@ -1,207 +1,155 @@
 import numpy as np
-
-import tracker
-from detector import Detector
 import cv2
+import matplotlib.pyplot as plt
+import time
+from detector import Detector
+import tracker
 
 if __name__ == '__main__':
+    # 初始化进入和离开计数
+    down_count_person = 0
+    up_count_person = 0
+    down_count_vehicle = 0
+    up_count_vehicle = 0
 
-    # 根据视频尺寸，填充一个polygon，供撞线计算使用
-    mask_image_temp = np.zeros((1080, 1920), dtype=np.uint8)
+    # 初始化计数列表
+    frame_up_counts_person = []
+    frame_down_counts_person = []
+    frame_up_counts_vehicle = []
+    frame_down_counts_vehicle = []
 
-    # 初始化2个撞线polygon
-    list_pts_blue = [[204, 305], [227, 431], [605, 522], [1101, 464], [1900, 601], [1902, 495], [1125, 379], [604, 437],
-                     [299, 375], [267, 289]]
-    ndarray_pts_blue = np.array(list_pts_blue, np.int32)
-    polygon_blue_value_1 = cv2.fillPoly(mask_image_temp, [ndarray_pts_blue], color=1)
-    polygon_blue_value_1 = polygon_blue_value_1[:, :, np.newaxis]
-
-    # 填充第二个polygon
-    mask_image_temp = np.zeros((1080, 1920), dtype=np.uint8)
-    list_pts_yellow = [[181, 305], [207, 442], [603, 544], [1107, 485], [1898, 625], [1893, 701], [1101, 568],
-                       [594, 637], [118, 483], [109, 303]]
-    ndarray_pts_yellow = np.array(list_pts_yellow, np.int32)
-    polygon_yellow_value_2 = cv2.fillPoly(mask_image_temp, [ndarray_pts_yellow], color=2)
-    polygon_yellow_value_2 = polygon_yellow_value_2[:, :, np.newaxis]
-
-    # 撞线检测用mask，包含2个polygon，（值范围 0、1、2），供撞线计算使用
-    polygon_mask_blue_and_yellow = polygon_blue_value_1 + polygon_yellow_value_2
-
-    # 缩小尺寸，1920x1080->960x540
-    polygon_mask_blue_and_yellow = cv2.resize(polygon_mask_blue_and_yellow, (960, 540))
-
-    # 蓝 色盘 b,g,r
-    blue_color_plate = [255, 0, 0]
-    # 蓝 polygon图片
-    blue_image = np.array(polygon_blue_value_1 * blue_color_plate, np.uint8)
-
-    # 黄 色盘
-    yellow_color_plate = [0, 255, 255]
-    # 黄 polygon图片
-    yellow_image = np.array(polygon_yellow_value_2 * yellow_color_plate, np.uint8)
-
-    # 彩色图片（值范围 0-255）
-    color_polygons_image = blue_image + yellow_image
-    # 缩小尺寸，1920x1080->960x540
-    color_polygons_image = cv2.resize(color_polygons_image, (960, 540))
-
-    # list 与蓝色polygon重叠
-    list_overlapping_blue_polygon = []
-
-    # list 与黄色polygon重叠
-    list_overlapping_yellow_polygon = []
-
-    # 进入数量
-    down_count = 0
-    # 离开数量
-    up_count = 0
+    # 初始化时间列表
+    time_stamps = []
+    start_time = time.time()
 
     font_draw_number = cv2.FONT_HERSHEY_SIMPLEX
-    draw_text_postion = (int(960 * 0.01), int(540 * 0.05))
+    draw_text_position = (int(960 * 0.01), int(540 * 0.05))
 
-    # 初始化 yolov5
+    # 初始化检测器
     detector = Detector()
 
+    # 初始化视频写入对象
+    fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    out = cv2.VideoWriter('output_video.avi', fourcc, 20.0, (960, 540))
+
     # 打开视频
-    capture = cv2.VideoCapture('./video/test.mp4')
-    # capture = cv2.VideoCapture('/mnt/datasets/datasets/towncentre/TownCentreXVID.avi')
+    # capture = cv2.VideoCapture('./video/test.mp4')
+    capture = cv2.VideoCapture('./video/IMG.mov')
+    # 用于跟踪已经统计过的目标ID的集合
+    counted_ids = set()
 
     while True:
         # 读取每帧图片
-        _, im = capture.read()
-        if im is None:
+        ret, im = capture.read()
+        if not ret:
             break
 
         # 缩小尺寸，1920x1080->960x540
         im = cv2.resize(im, (960, 540))
-
         list_bboxs = []
+
+
+        # 检测目标
         bboxes = detector.detect(im)
 
-        # 如果画面中 有bbox
-        if len(bboxes) > 0:
-            list_bboxs = tracker.update(bboxes, im)
+        # 更新目标追踪信息
+        bboxes2draw = tracker.update(bboxes, im)
 
-            # 画框
-            # 撞线检测点，(x1，y1)，y方向偏移比例 0.0~1.0
-            output_image_frame = tracker.draw_bboxes(im, list_bboxs, line_thickness=None)
-            pass
-        else:
-            # 如果画面中 没有bbox
-            output_image_frame = im
-        pass
+        # 统计人流和车流数量
+        for x1, y1, x2, y2, label, track_id in bboxes2draw:
+            # 如果跟踪ID已经统计过，则跳过
+            if track_id in counted_ids:
+                continue
 
-        # 输出图片
-        output_image_frame = cv2.add(output_image_frame, color_polygons_image)
+            # 绘制边界框和跟踪ID
+            cv2.rectangle(im, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(im, f'Track ID: {track_id}', (x1, y1 - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        if len(list_bboxs) > 0:
-            # ----------------------判断撞线----------------------
-            for item_bbox in list_bboxs:
-                x1, y1, x2, y2, label, track_id = item_bbox
-
-                # 撞线检测点，(x1，y1)，y方向偏移比例 0.0~1.0
-                y1_offset = int(y1 + ((y2 - y1) * 0.6))
-
-                # 撞线的点
-                y = y1_offset
-                x = x1
-
-                if polygon_mask_blue_and_yellow[y, x] == 1:
-                    # 如果撞 蓝polygon
-                    if track_id not in list_overlapping_blue_polygon:
-                        list_overlapping_blue_polygon.append(track_id)
-                    pass
-
-                    # 判断 黄polygon list 里是否有此 track_id
-                    # 有此 track_id，则 认为是 外出方向
-                    if track_id in list_overlapping_yellow_polygon:
-                        # 外出+1
-                        up_count += 1
-
-                        print(f'类别: {label} | id: {track_id} | 上行撞线 | 上行撞线总数: {up_count} | 上行id列表: {list_overlapping_yellow_polygon}')
-
-                        # 删除 黄polygon list 中的此id
-                        list_overlapping_yellow_polygon.remove(track_id)
-
-                        pass
-                    else:
-                        # 无此 track_id，不做其他操作
-                        pass
-
-                elif polygon_mask_blue_and_yellow[y, x] == 2:
-                    # 如果撞 黄polygon
-                    if track_id not in list_overlapping_yellow_polygon:
-                        list_overlapping_yellow_polygon.append(track_id)
-                    pass
-
-                    # 判断 蓝polygon list 里是否有此 track_id
-                    # 有此 track_id，则 认为是 进入方向
-                    if track_id in list_overlapping_blue_polygon:
-                        # 进入+1
-                        down_count += 1
-
-                        print(f'类别: {label} | id: {track_id} | 下行撞线 | 下行撞线总数: {down_count} | 下行id列表: {list_overlapping_blue_polygon}')
-
-                        # 删除 蓝polygon list 中的此id
-                        list_overlapping_blue_polygon.remove(track_id)
-
-                        pass
-                    else:
-                        # 无此 track_id，不做其他操作
-                        pass
-                    pass
+            # 统计人流和车流
+            if label == 'person':
+                if y1 < 540 / 2:
+                    up_count_person += 1
                 else:
-                    pass
-                pass
+                    down_count_person += 1
+            elif label == 'vehicle':
+                if y1 < 540 / 2:
+                    up_count_vehicle += 1
+                else:
+                    down_count_vehicle += 1
 
-            pass
+            # 将已统计过的跟踪ID添加到集合中
+            counted_ids.add(track_id)
 
-            # ----------------------清除无用id----------------------
-            list_overlapping_all = list_overlapping_yellow_polygon + list_overlapping_blue_polygon
-            for id1 in list_overlapping_all:
-                is_found = False
-                for _, _, _, _, _, bbox_id in list_bboxs:
-                    if bbox_id == id1:
-                        is_found = True
-                        break
-                    pass
-                pass
-
-                if not is_found:
-                    # 如果没找到，删除id
-                    if id1 in list_overlapping_yellow_polygon:
-                        list_overlapping_yellow_polygon.remove(id1)
-                    pass
-                    if id1 in list_overlapping_blue_polygon:
-                        list_overlapping_blue_polygon.remove(id1)
-                    pass
-                pass
-            list_overlapping_all.clear()
-            pass
-
-            # 清空list
-            list_bboxs.clear()
-
-            pass
+        # 如果画面中有bbox
+        if bboxes:
+            list_bboxs = tracker.update(bboxes, im)
+            # 画框
+            output_image_frame = tracker.draw_bboxes(
+                im, list_bboxs, line_thickness=None)
         else:
-            # 如果图像中没有任何的bbox，则清空list
-            list_overlapping_blue_polygon.clear()
-            list_overlapping_yellow_polygon.clear()
-            pass
-        pass
+            # 如果画面中没有bbox
+            output_image_frame = im
+        # 当前帧上下行目标数量
+        current_up_count_person = sum(
+            1 for bbox in list_bboxs if bbox[1] < 540 / 2 and bbox[4] == 'person')  # 上半部分人数量
+        current_down_count_person = sum(
+            1 for bbox in list_bboxs if bbox[1] >= 540 / 2 and bbox[4] == 'person')  # 下半部分人数量
+        current_up_count_vehicle = sum(
+            1 for bbox in list_bboxs if bbox[1] < 540 / 2 and bbox[4] == 'vehicle')  # 上半部分车数量
+        current_down_count_vehicle = sum(
+            1 for bbox in list_bboxs if bbox[1] >= 540 / 2 and bbox[4] == 'vehicle')  # 下半部分车数量
 
-        text_draw = 'DOWN: ' + str(down_count) + \
-                    ' , UP: ' + str(up_count)
-        output_image_frame = cv2.putText(img=output_image_frame, text=text_draw,
-                                         org=draw_text_postion,
+
+
+        # 收集当前时间和帧计数
+        current_time = time.time() - start_time
+        time_stamps.append(current_time)
+        frame_up_counts_person.append(current_up_count_person)
+        frame_down_counts_person.append(current_down_count_person)
+        frame_up_counts_vehicle.append(current_up_count_vehicle)
+        frame_down_counts_vehicle.append(current_down_count_vehicle)
+
+
+        # 绘制统计信息到帧上
+        text_draw = f'DOWN: Person {down_count_person} , Vehicle {down_count_vehicle} | UP: Person {up_count_person} , Vehicle {up_count_vehicle}'
+        output_image_frame = cv2.putText(img=im, text=text_draw,
+                                         org=draw_text_position,
                                          fontFace=font_draw_number,
                                          fontScale=1, color=(255, 255, 255), thickness=2)
 
+        # 显示结果帧
         cv2.imshow('demo', output_image_frame)
+        if out.isOpened():  # 检查视频写入对象是否成功打开
+            out.write(output_image_frame)
         cv2.waitKey(1)
-
-        pass
-    pass
 
     capture.release()
     cv2.destroyAllWindows()
+
+    # 生成柱状图
+    plt.figure(figsize=(10, 5))
+    plt.bar(['Up Person', 'Down Person', 'Up Vehicle', 'Down Vehicle'],
+            [up_count_person, down_count_person,
+             up_count_vehicle, down_count_vehicle],
+            color=['blue', 'cyan', 'green', 'yellow'])
+    plt.title('Total Up and Down Counts for Person and Vehicle')
+    plt.savefig('total_bar_chart.png')
+    plt.show()
+
+    # 生成折线图
+    plt.figure(figsize=(10, 5))
+    plt.plot(time_stamps, frame_up_counts_person,
+             label='Up Person per Frame', color='blue')
+    plt.plot(time_stamps, frame_down_counts_person,
+             label='Down Person per Frame', color='cyan')
+    plt.plot(time_stamps, frame_up_counts_vehicle,
+             label='Up Vehicle per Frame', color='green')
+    plt.plot(time_stamps, frame_down_counts_vehicle,
+             label='Down Vehicle per Frame', color='yellow')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Counts per Frame')
+    plt.title('Counts per Frame Over Time for Person and Vehicle')
+    plt.legend()
+    plt.savefig('frame_line_chart.png')
+    plt.show()
